ede0f2e862eeb96d2a47e071f709a623bed4cba4
Tudor Radoni <tudor.radoni@gmail.com>
2024-09-28 10:40:59 +0300

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true
   },
-  site: 'https://lexingtonthemes.com',
+  site: 'lattecrafter.github.io',
   integrations: [sitemap(), mdx()]
 });
